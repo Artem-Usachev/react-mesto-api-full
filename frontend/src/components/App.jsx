6b7390ab4acbc74157e1cc3e1baed42b1a530cbf
усачёв артём <bbbiii.444@yandex.ru>
2022-01-23 00:29:17 +0500

    rev likes

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -42,9 +42,10 @@ const App = () => {
 
     const handleCardLike = (card) => {
         const isLiked = card.likes.some((i) => i._id === currentUser._id)
-        console.log(card)
+
         api.changeLikeCardStatus(card, isLiked)
             .then((newCard) => {
+                console.log(newCard)
                 setCards((cards) => cards.map((c) => (c._id === card._id ? newCard : c)))
             })
             .catch((err) => console.log(err))
